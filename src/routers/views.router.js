73e6d226075281d1e4aca29c6c5e74cd22b5dee2
Tadeo Antonino <tadeoantonino98@gmail.com>
2022-12-23 14:29:19 -0300

    desafio 4 websockets handlebars, faltan funciones de pre entrega 1

--- src/routers/views.router.js
+++ src/routers/views.router.js
@@ -15,7 +15,7 @@ viewsRouter.get("/", (req, res) => {
 viewsRouter.get("/realtimeproducts", (req, res) => {
     try {
         const productsList = ProductManager.getProducts();
-        res.status(200).render('realTime', {products: productsList})
+        res.status(200).render("realTime", {products: productsList})
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
