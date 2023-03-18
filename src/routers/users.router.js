f8cce0760d0390be43f0087e6017ec1da888589c
Tadeo Antonino <tadeoantonino98@gmail.com>
2023-03-18 02:23:35 -0300

    current

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -8,5 +8,6 @@ router.post("/", UserController.createUser);
 router.get("/:email", auth, UserController.getUser);
 router.put("/updateUser/:email", auth, UserController.updateUser);
 router.put("/updatePassword/:email", auth, UserController.updatePassword);
+router.get("/current", auth, UserController.getUser); //No entendí bien si es así lo que pide la /current , lo entendí como que te traiga el usuario
 
 export default router;
